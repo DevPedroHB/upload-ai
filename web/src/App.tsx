613770c593e3e3375642fb528f7ce6b89b98dfd1
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-09-12 10:51:51 -0300

    🛠️ Configurado o Tailwind CSS e shadcn/ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,15 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <div className="px-6">
+        <h1>upload.ai</h1>
+        <div>
+          <span>Desenvolvido com 💜 no NLW da Rocketseat</span>
+          <Button>Github</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
